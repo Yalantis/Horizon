b6612bef50fff7059083e862d0f5c3fcf7f0ebf4
Roman Kozlov <vafli9@gmail.com>
2016-02-29 12:24:49 +0200

    ready for deployment

--- app/src/main/java/com/yalantis/soundwaves/DemoActivity.java
+++ app/src/main/java/com/yalantis/soundwaves/DemoActivity.java
@@ -1,15 +1,113 @@
 package com.yalantis.soundwaves;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yalantis.audio.lib.AudioUtil;
+import com.yalantis.siri.util.Eqwaves;
 
 public class DemoActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSION_RECORD_AUDIO = 1;
+
+    private static final int RECORDER_SAMPLE_RATE = 44100;
+    private static final int RECORDER_CHANNELS = 1;
+    private static final int RECORDER_ENCODING_BIT = 16;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int MAX_DECIBELS = 120;
+
+    private AudioRecord audioRecord;
+    private Eqwaves mEqwaves;
+    private GLSurfaceView glSurfaceView;
+
+    private Thread recordingThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+        glSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface);
+        mEqwaves = new Eqwaves(glSurfaceView, getResources().getColor(R.color.background),
+                RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_ENCODING_BIT);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+        checkPermissionsAndStart();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        glSurfaceView.onPause();
+        if (audioRecord != null) {
+            audioRecord.release();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_PERMISSION_RECORD_AUDIO:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkPermissionsAndStart();
+                } else {
+                    finish();
+                }
+        }
+
+    }
+
+    private void checkPermissionsAndStart() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_PERMISSION_RECORD_AUDIO);
+        } else {
+            initRecorder();
+            if (audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
+                startRecording();
+            }
+        }
+    }
+
+    private void initRecorder() {
+        int bufferSize = AudioRecord.getMinBufferSize(RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING, bufferSize);
+        AudioUtil.initProcessor(RECORDER_SAMPLE_RATE, RECORDER_CHANNELS, RECORDER_ENCODING_BIT);
+
+        recordingThread = new Thread("recorder") {
+            @Override
+            public void run() {
+                super.run();
+                int bufferSize = 2 * AudioRecord.getMinBufferSize(RECORDER_SAMPLE_RATE,
+                        RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING);
+                byte[] buffer = new byte[bufferSize];
+                while (audioRecord.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING
+                        && audioRecord.read(buffer, 0, bufferSize) != -1) {
+                    mEqwaves.updateView(buffer);
+                }
+                AudioUtil.disposeProcessor();
+            }
+        };
+    }
+
+    private void startRecording() {
+        if (audioRecord != null) {
+            audioRecord.startRecording();
+        }
+        recordingThread.start();
+    }
 }
